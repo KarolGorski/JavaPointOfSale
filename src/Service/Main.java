2a7612933db95f27e6342dde097eb18055ddd0a0
KarolGorski <goralk2870@gmail.com>
2017-05-03 21:50:25 +0200

    It's working nicely <3

--- src/Service/Main.java
+++ src/Service/Main.java
@@ -1,22 +1,31 @@
 package Service;
-import LCD.LCDscreen;
-
-
 
 /*
  * @author Karol
  */
+
+/*
+Exiting program is possible through hitting 
+ESC button on LCDscreen as well as typing "exit" into scanner
+*/
+
+/*List of hardcoded products into database. To test put barcodes into scanner:
+Legend:
+barcode, name, price
+"1","Butter",5.0
+"12312","Bread",3.5
+"2135","Orange Juice",6.0
+"5321","Sausage",6.0
+
+You can add some in Database.DatabaseMock ;) 
+*/
+
 public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run(){
-            new LCDscreen().setVisible(true);
-            }
-            
-        });
+        new ServiceSetup();
+       
     }
-    
     
 }
